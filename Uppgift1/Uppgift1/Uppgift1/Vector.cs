ff0a80b86aeb42b98b32fc201405c76b2c7a6da8
Zyrst <zajreg@gmail.com>
2015-11-06 14:22:29 +0100

    Början på vector klass

---
+++ Uppgift1/Uppgift1/Uppgift1/Vector.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uppgift1
+{
+    
+    public class Vector
+    {
+        private float mX;
+        private float mY;
+
+
+        public static Vector createVector(float x, float y)
+        {
+            Vector v = new Vector();
+            v.mX = x;
+            v.mY = y;
+            return v;
+        }
+        
+       public static Vector operator +(Vector v1, Vector v2)
+       {
+            Vector v = new Vector();
+            v.mX = v1.mX + v2.mX;
+            v.mY = v1.mY + v2.mY;
+            return v;
+       }
+       
+        public static Vector operator -(Vector v1, Vector v2)
+        {
+            Vector v = new Vector();
+            v.mX = v1.mX - v2.mX;
+            v.mY = v1.mY - v2.mY;
+            return v;
+        }
+
+        public static float operator *(Vector v1, Vector v2)
+        {
+            
+            return (v1.mX * v2.mX) + (v1.mY * v2.mY);
+        }
+    }
+}
